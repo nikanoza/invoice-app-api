e48cb269bea1e184c9e1c5feaa3ecc5e1e682648
hilu <nikanozadze1010@gmail.com>
2022-11-22 23:05:26 +0400

    feat: add router and cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import connect from "./config/mongo.js";
+import invoiceRouter from "router.js";
 
 dotenv.config();
 connect();
 
 const app = express();
+
+app.use("/api", cors(), invoiceRouter);
 
 app.listen(process.env.PORT || 4000);
---
+++ src/router.ts
@@ -0,0 +1,5 @@
+import express from "express";
+
+const invoiceRouter = express.Router();
+
+export default invoiceRouter;
