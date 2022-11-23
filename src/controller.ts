38aae4098a85f275d77aa8b0a034547de1dc09bc
hilu <nikanozadze1010@gmail.com>
2022-11-24 00:22:48 +0400

    feat: create new invoice post request function

--- src/controller.ts
+++ src/controller.ts
@@ -1,8 +1,67 @@
 import { Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import invoiceSchema from "invoice-schema.js";
 import Invoice from "./Invoice.js";
 
 export const getInvoices = async (req: Request, res: Response) => {
   const data = await Invoice.find();
 
   return res.status(200).json(data);
+};
+
+export const createInvoice = async (req: Request, res: Response) => {
+  const { body } = req;
+
+  const validator = invoiceSchema(body);
+  const { value, error } = validator.validate(body);
+
+  if (error) {
+    return res.status(422).json(error.details);
+  }
+
+  const {
+    createdAt,
+    paymentDue,
+    description,
+    paymentTerms,
+    clientName,
+    clientEmail,
+    status,
+    senderAddress,
+    clientAddress,
+    items,
+    total,
+  } = value;
+
+  const id = uuidv4();
+
+  const newInvoice = {
+    id,
+    createdAt,
+    paymentDue,
+    description,
+    paymentTerms,
+    clientName,
+    clientEmail,
+    status,
+    senderAddress: {
+      street: senderAddress.street,
+      city: senderAddress.city,
+      postCode: senderAddress.postCode,
+      country: senderAddress.country,
+    },
+    clientAddress: {
+      street: clientAddress.street,
+      city: clientAddress.city,
+      postCode: clientAddress.postCode,
+      country: clientAddress.country,
+    },
+    items,
+    total,
+  };
+
+  await Invoice.create({ ...newInvoice });
+
+  return res.status(201).json({ ...newInvoice });
 };
--- src/invoice-schema.ts
+++ src/invoice-schema.ts
@@ -1,7 +1,7 @@
 import Joi from "joi";
 import { AddressT, ItemT, NewInvoice } from "types";
 
-const addInvoiceSchema = (data: NewInvoice) => {
+const invoiceSchema = (data: NewInvoice) => {
   return Joi.object<NewInvoice>({
     createdAt: Joi.string()
       .pattern(/^\d{4}[-]\d{2}[-]\d{2}$/)
@@ -104,3 +104,5 @@ const addInvoiceSchema = (data: NewInvoice) => {
     }),
   });
 };
+
+export default invoiceSchema;
